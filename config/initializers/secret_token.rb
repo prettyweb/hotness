82a3fe7fadfc50b5f22e21404b1d3b09f562384b
Quan Sun <qsun@pardiff.com>
2011-08-28 12:11:31 +1000

    init - rails
    Signed-off-by: Quan Sun <qsun@pardiff.com>

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hotness::Application.config.secret_token = '8b82716ed01adfbd3bc1b5e0a0b84b714a32e064887d9d4eb7333b66e04186d55755a73b08750d7ffb1965665ea8628df40ffb54d0d22d4b0837022359c5f857'
